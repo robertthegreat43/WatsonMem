4bdece8ce406f98b513de0368a0e759b6ff2994a
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-18 02:33:19 -0500

    Update settings.py

--- WatsonMemorial/settings.py
+++ WatsonMemorial/settings.py
@@ -17,7 +17,7 @@ from django.urls import path
 from django.conf.global_settings import MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
