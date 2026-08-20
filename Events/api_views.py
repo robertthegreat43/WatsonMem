73ae3534f54b98b5313196991ad51d003f57b665
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-19 22:52:31 -0500

    Remove unused imports from api_views.py

--- Events/api_views.py
+++ Events/api_views.py
@@ -1,8 +1,1 @@
-import os
-import cv2
-from django.conf import settings
-from django.http import FileResponse, Http404, JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
-# Import the recording variables from views.py
