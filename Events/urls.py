e250cfb9589ce674a5b5e9d33cd32e47dcd40425
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-19 20:59:20 -0500

    Update urls.py

--- Events/urls.py
+++ Events/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin, auth
 from django.urls import path, include
 from . import views, api_views
-from .api_views import start_recording, stop_recording, list_recordings, download_video
-from .views import video_feed
+from Events.api_views import start_recording, stop_recording, list_recordings, download_video
+from Events.views import video_feed
+
+
+
 
 
 urlpatterns = [
