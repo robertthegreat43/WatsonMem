c65693badeeb7381037797df6223f66de21310a6
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-19 20:07:58 -0500

    Reformat urls.py for consistency and clarity

--- Events/urls.py
+++ Events/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path("accounts/password_reset/done/", auth.views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path("accounts/reset/done/", auth.views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
-    path('api/all_events/', api_views.EventsList.as_view(), name="all_events"),
+
     path('video_feed/', views.video_feed, name='video_feed'),
     path('api/start_recording/', api_views.start_recording),
     path('api/stop_recording/', api_views.stop_recording),
