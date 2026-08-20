95cd30e607a224ac6b723aa747f4381ba2e33855
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-19 19:40:39 -0500

    Update settings.py

--- WatsonMemorial/settings.py
+++ WatsonMemorial/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
-from django.urls import path
 from django.conf.global_settings import MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
