044b92808a1ecb7b59fdc6d3e7146b8c1500a7a7
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-19 22:41:47 -0500

    Reformat urls.py with consistent imports and paths

--- Events/urls.py
+++ Events/urls.py
@@ -18,10 +18,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/password_reset/done/", auth.views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path("accounts/reset/done/", auth.views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
-    path('api/start_recording/', api_views.start_recording),
-    path('api/stop_recording/', api_views.stop_recording),
-    path('api/videos/', list_recordings),
-    path('api/download/<str:filename>/', api_views.download_video),
     path('api/biblechat/', views.biblechat_api, name='biblechat_api'),
     path('biblechat/', views.biblechat, name='biblechat'),
 ]
