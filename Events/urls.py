6feadb8469dd78cde7292d8f8dc6ede67df0eb36
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-19 23:19:49 -0500

    Reorganize imports and maintain urlpatterns

--- Events/urls.py
+++ Events/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin, auth
 from django.urls import path, include
-from . import views, api_views
+from . import views
 
 urlpatterns = [
     path('', views.home),
