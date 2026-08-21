d13948b449a7d2c4b47f1b2f03c02b4895ee91f9
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-21 17:03:10 -0500

    Update Django settings with new configurations

--- WatsonMemorial/settings.py
+++ WatsonMemorial/settings.py
@@ -62,18 +62,19 @@ ROOT_URLCONF = 'WatsonMemorial.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],   
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'WatsonMemorial.wsgi.application'
 
